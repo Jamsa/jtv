55e6171d7895a10f95ae47bf6538b4ce79193752
zhujie <jiemao@gmail.com>
2018-07-18 11:17:46 +0800

    调整客户端主窗口布局

--- client/src/main/scala/gui.scala
+++ client/src/main/scala/gui.scala
@@ -24,18 +24,61 @@ object MainFrame extends JFrame with Observer{
     if(initFlag)return
     setTitle("JTV")
     setSize(300,100)
+    setLocationRelativeTo(null)
     //setAlwaysOnTop(true)
     val contentPanel = getContentPane
-    contentPanel.setLayout(new FlowLayout())
-    contentPanel.add(new JLabel("会话："))
+    val layout = new GridBagLayout
+
+    //contentPanel.setLayout(new FlowLayout())
+    contentPanel.setLayout(layout)
+    val l1 = new JLabel("会话：")
+    contentPanel.add(l1)
     contentPanel.add(sessionId)
     sessionId.setEditable(false)
-    contentPanel.add(new JLabel("密码："))
+    val l2 = new JLabel("密码：")
+    contentPanel.add(l2)
     contentPanel.add(sessionPassword)
     sessionPassword.setEditable(false)
     contentPanel.add(connectBtn)
 
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+
+    //元素布局
+    val s = new GridBagConstraints
+    s.fill = GridBagConstraints.BOTH
+    //该方法是为了设置如果组件所在的区域比组件本身要大时的显示情况
+    //NONE：不调整组件大小。
+    //HORIZONTAL：加宽组件，使它在水平方向上填满其显示区域，但是不改变高度。
+    //VERTICALss：加高组件，使它在垂直方向上填满其显示区域，但是不改变宽度。
+    //BOTH：使组件完全填满其显示区域。
+    s.gridwidth=1 //该方法是设置组件水平所占用的格子数，如果为0，就说明该组件是该行的最后一个
+    s.weightx = 0 //该方法设置组件水平的拉伸幅度，如果为0就说明不拉伸，不为0就随着窗口增大进行拉伸，0到1之间
+    s.weighty=0 //该方法设置组件垂直的拉伸幅度，如果为0就说明不拉伸，不为0就随着窗口增大进行拉伸，0到1之间
+
+    s.insets = new Insets(0,10,0,0)
+    s.anchor = GridBagConstraints.EAST
+    layout.setConstraints(l1, s) //设置组件
+
+    s.anchor = GridBagConstraints.WEST
+    s.insets = new Insets(0,0,0,10)
+    s.gridwidth=0
+    s.weightx=1
+    layout.setConstraints(sessionId,s)
+
+    s.insets = new Insets(0,10,0,0)
+    s.anchor = GridBagConstraints.EAST
+    s.gridwidth=1
+    s.weightx=0
+    layout.setConstraints(l2,s)
+
+    s.anchor = GridBagConstraints.WEST
+    s.insets = new Insets(0,0,0,10)
+    s.gridwidth=0
+    s.weightx=1
+    layout.setConstraints(sessionPassword,s)
+
+    s.insets = new Insets(0,10,0,10)
+    layout.setConstraints(connectBtn,s)
 
     MainFrameManager.addObserver(this)
 
